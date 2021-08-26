0c2da3bf087106b6cdb44987c455e8fcfba03afe
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-08-26 17:38:02 -0400

    Segundo commit, estrutura inicial.

---
+++ lib/Jogo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          //1) texto
+          //2) texto resultado
+          //3) imagem
+          //4) linha 3 imagens
+          Padding(
+            padding: EdgeInsets.only(top:32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("imagens/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top:32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("imagens/pedra.png", height: 100,),
+              Image.asset("imagens/papel.png", height: 100,),
+              Image.asset("imagens/tesoura.png", height: 100,),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
